01d64532e1c52fbcf322a49eac28c6329cf25853
Sayeed Najeeb <48534405+Sayeed-SCE@users.noreply.github.com>
2022-06-10 16:10:33 -0700

    made changes on AppClass.js file

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,26 +1,37 @@
 import React from 'react'
+import axios from 'axios'
 
+const URL = 'http://localhost:9000/api/result'
 // Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+// const initialMessage = ''
+// const initialEmail = ''
+// const initialSteps = 0
+// const initialIndex = 4 // the index the "B" is at
 
-const initialState = {
-  message: initialMessage,
-  email: initialEmail,
-  index: initialIndex,
-  steps: initialSteps,
-}
+// const initialState = {
+//   message: initialMessage,
+//   email: initialEmail,
+//   index: initialIndex,
+//   steps: initialSteps,
+// }
 
 export default class AppClass extends React.Component {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  state ={
+    message: " ",
+    email: " ",
+    totalMoves: 0,
+    square:4,
+  }
 
   getXY = () => {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
+    let x = 1;
+    let y = 2;
   }
+  
 
   getXYMessage = () => {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
@@ -30,6 +41,12 @@ export default class AppClass extends React.Component {
 
   reset = () => {
     // Use this helper to reset all states to their initial values.
+    this.setState({
+    message: " ",
+    email: " ",
+    totalMoves: 0,
+    square: 4,  
+    });
   }
 
   getNextIndex = (direction) => {
@@ -41,14 +58,28 @@ export default class AppClass extends React.Component {
   move = (evt) => {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    
   }
 
   onChange = (evt) => {
     // You will need this to update the value of the input.
+    this.setState({ ...this.state, email: evt.target.value })
   }
+  upClickHandler = (evt) => { this.setState({ ...this.state, square: evt.target.value.up}) };
+  downClickHandler = (evt) => { this.setState({ ...this.state, square: evt.target.value.down}) };
+  rightClickHandler = (evt) => { this.setState({ ...this.state, square: evt.target.value.right}) }
+  leftClickHandler = (evt) => { this.setState({ ...this.state, square: evt.target.value.left}) }
+
 
   onSubmit = (evt) => {
     // Use a POST request to send a payload to the server.
+    axios.post(URL, {email: this.state.email} )
+      .then(res => {
+        console.log(res)
+        this.setState({ ...this.state, email: ''})
+
+      })
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -56,8 +87,8 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">{`Coordinates ${this.state.coordinates}`}</h3>
+          <h3 id="steps">{`You moved ${this.state.totalMoves} times`}</h3>
         </div>
         <div id="grid">
           {
@@ -69,17 +100,22 @@ export default class AppClass extends React.Component {
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={this.leftClickHandler}id="left">LEFT</button>
+          <button onClick={this.upClickHandler}id="up">UP</button>
+          <button onClick={this.rightClickHandler}id="right">RIGHT</button>
+          <button onClick={this.downClickHandler}id="down">DOWN</button>
+          <button onClick={this.reset}id="reset">reset</button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
+          <input value={this.state.email} 
+                  onChange={this.onChange}
+                  id="email" type="email" 
+                  placeholder="type email">
+          </input>
+
           <input id="submit" type="submit"></input>
         </form>
       </div>
