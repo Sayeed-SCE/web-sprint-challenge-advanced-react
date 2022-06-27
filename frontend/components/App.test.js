ae323611125c266bf95d95b4576154c384f59e33
Sayeed Najeeb <48534405+Sayeed-SCE@users.noreply.github.com>
2022-06-27 16:22:18 -0700

    completed AppClass component

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,17 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import AppClass from './AppClass';
+
 // Write your tests here
 test('sanity', () => {
+ 
   expect(true).toBe(true)
+})
+
+test('render the header to be exact Welcome to the Grid', ()=>{
+  render(<AppClass.js/>)
+  const header = screen.queryByText(/welcome to the grid/i);
+  expect(header).toBeInTheDocument();
 })
--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,123 +1,273 @@
-import React from 'react'
-import axios from 'axios'
-
-const URL = 'http://localhost:9000/api/result'
-// Suggested initial states
-// const initialMessage = ''
-// const initialEmail = ''
-// const initialSteps = 0
-// const initialIndex = 4 // the index the "B" is at
-
-// const initialState = {
-//   message: initialMessage,
-//   email: initialEmail,
-//   index: initialIndex,
-//   steps: initialSteps,
-// }
+import React from "react";
+import axios from "axios";
 
 export default class AppClass extends React.Component {
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
-  state ={
-    message: " ",
-    email: " ",
+  state = {
+    message: "",
+    email: "",
+    coordinates: "(2, 2)",
     totalMoves: 0,
-    square:4,
-  }
+    square: ["", "", "", "", "B", "", "", "", ""],
+  };
 
-  getXY = () => {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
-  
-  }
-  
-
-  getXYMessage = () => {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
-  }
-
-  reset = () => {
-    // Use this helper to reset all states to their initial values.
-    this.setState({
-    message: " ",
-    email: " ",
-    totalMoves: 0,
-    square: 4,  
-    });
-  }
-
-  getNextIndex = (direction) => {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
-  }
-
-  move = (evt) => {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
-    
-  }
-
-  onChange = (evt) => {
-    // You will need this to update the value of the input.
-    this.setState({ ...this.state, email: evt.target.value })
-  }
-  upClickHandler = (evt) => { this.setState({ ...this.state, square: evt.target.value.up}) };
-  downClickHandler = (evt) => { this.setState({ ...this.state, square: evt.target.value.down}) };
-  rightClickHandler = (evt) => { this.setState({ ...this.state, square: evt.target.value.right}) }
-  leftClickHandler = (evt) => { this.setState({ ...this.state, square: evt.target.value.left}) }
-
-
-  onSubmit = (evt) => {
-    // Use a POST request to send a payload to the server.
-    axios.post(URL, {email: this.state.email} )
-      .then(res => {
-        console.log(res)
-        this.setState({ ...this.state, email: ''})
-
+  postNewEmail = () => {
+    axios
+      .post("http://localhost:9000/api/result", {
+        x: this.coordinatesFinder()[1],
+        y: this.coordinatesFinder()[4],
+        email: this.state.email,
+        steps: this.state.totalMoves,
       })
-      .catch(err => console.log(err))
-  }
+
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          message: res.data.message,
+          email: "",
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          ...this.state,
+          message: err.response.data.message,
+        });
+      });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.postNewEmail();
+    this.setState({
+      ...this.state,
+      email: "",
+    });
+  };
+
+  coordinatesFinder = () => {
+    const address = [
+      "(1, 1)",
+      "(2, 1)",
+      "(3, 1)",
+      "(1, 2)",
+      "(2, 2)",
+      "(3, 2)",
+      "(1, 3)",
+      "(2, 3)",
+      "(3, 3)",
+    ];
+
+    const newArray = this.state.square;
+    const val = newArray.indexOf("B");
+    const currentSquare = address.find((current, index) => {
+      if (index === val) {
+        return current;
+      }
+    });
+    return currentSquare;
+  };
+
+  resetState = () => {
+    return this.setState({
+      ...this.state,
+      coordinates: "(2, 2)",
+      totalMoves: 0,
+      square: ["", "", "", "", "B", "", "", "", ""],
+      message: "",
+      email: "",
+    });
+  };
+  handleEmail = (e) => {
+    const { value } = e.target;
+    this.setState({ ...this.state, email: value });
+  };
+
+  handleChangeLeft = () => {
+    const helperFunc = (arr, from, to) => {
+      let ele = arr[from];
+      arr.splice(from, 1);
+      arr.splice(to, 0, ele);
+    };
+    const newArray = this.state.square;
+    const val = newArray.indexOf("B");
+
+    if (val === newArray[0] || newArray[0] || newArray[3] || newArray[6]) {
+      return this.setState({
+        ...this.state,
+        coordinates: this.coordinatesFinder(),
+        totalMoves: this.state.totalMoves,
+        message: "You can't go left",
+      });
+    } else {
+      helperFunc(newArray, val, val - 1);
+      this.setState({
+        ...this.state,
+        coordinates: this.coordinatesFinder(),
+        totalMoves: this.state.totalMoves + 1,
+        message: "",
+        square: newArray,
+      });
+    }
+  };
+
+  handleChangeRight = () => {
+    const helperFunc = (arr, from, to) => {
+      let ele = arr[from];
+      arr.splice(from, 1);
+      arr.splice(to, 0, ele);
+    };
+    const newArray = this.state.square;
+    const val = newArray.indexOf("B");
+
+    if (val === newArray[1] || newArray[5] || newArray[8] || newArray[2]) {
+      return this.setState({
+        ...this.state,
+        coordinates: this.coordinatesFinder(),
+        totalMoves: this.state.totalMoves,
+        message: "You can't go right",
+      });
+    } else {
+      helperFunc(newArray, val, val + 1);
+      this.setState({
+        ...this.state,
+        coordinates: this.coordinatesFinder(),
+        totalMoves: this.state.totalMoves + 1,
+        message: "",
+        square: newArray,
+      });
+    }
+  };
+
+  handleChangeUp = () => {
+    const helperFunc = (arr, from, to) => {
+      let ele = arr[from];
+      arr.splice(from, 1);
+      arr.splice(to, 0, ele);
+    };
+    const newArray = this.state.square;
+    const val = newArray.indexOf("B");
+
+    if (val === newArray[0] || newArray[0] || newArray[1] || newArray[2]) {
+      return this.setState({
+        ...this.state,
+        coordinates: this.coordinatesFinder(),
+        totalMoves: this.state.totalMoves,
+        message: "You can't go up",
+      });
+    } else {
+      helperFunc(newArray, val, val - 3);
+      this.setState({
+        ...this.state,
+        coordinates: this.coordinatesFinder(),
+        totalMoves: this.state.totalMoves + 1,
+        message: "",
+        square: newArray,
+      });
+    }
+  };
+
+  handleChangeDown = () => {
+    const helperFunc = (arr, from, to) => {
+      let ele = arr[from];
+      arr.splice(from, 1);
+      arr.splice(to, 0, ele);
+    };
+    const newArray = this.state.square;
+    const val = newArray.indexOf("B");
+
+    if (val === newArray[6] || newArray[6] || newArray[7] || newArray[8]) {
+      return this.setState({
+        ...this.state,
+        coordinates: this.coordinatesFinder(),
+        totalMoves: this.state.totalMoves,
+        message: "You can't go down",
+      });
+    } else {
+      helperFunc(newArray, val, val + 3);
+      this.setState({
+        ...this.state,
+        coordinates: this.coordinatesFinder(),
+        totalMoves: this.state.totalMoves + 1,
+        message: "",
+        square: newArray,
+      });
+    }
+  };
+
+  addMoves = () => {
+    return this.setState({
+      ...this.state,
+
+      totalMoves: this.state.totalMoves + 1,
+    });
+  };
 
   render() {
-    const { className } = this.props
+    const { className } = this.props;
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinate: (2,2)</h3>
-          <h3 id="steps">{`You moved ${this.state.totalMoves} times`}</h3>
+          <h3 id="coordinates">Coordinates {this.state.coordinates}</h3>
+          <h3 id="steps">
+            You moved{" "}
+            {this.state.totalMoves === 1
+              ? `${this.state.totalMoves} time`
+              : `${this.state.totalMoves} times`}
+          </h3>
         </div>
         <div id="grid">
-          {
-            [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
-              </div>
-            ))
-          }
+          {this.state.square.map((val, idx) => {
+            if (val === "B") {
+              return (
+                <div key={idx} className="square active">
+                  {val}
+                </div>
+              );
+            } else {
+              return <div key={idx} className="square"></div>;
+            }
+          })}
         </div>
         <div className="info">
           <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button onClick={this.leftClickHandler}id="left">LEFT</button>
-          <button onClick={this.upClickHandler}id="up">UP</button>
-          <button onClick={this.rightClickHandler}id="right">RIGHT</button>
-          <button onClick={this.downClickHandler}id="down">DOWN</button>
-          <button onClick={this.reset}id="reset">reset</button>
+          <button onClick={this.handleChangeLeft} id="left">
+            LEFT
+          </button>
+          <button onClick={this.handleChangeUp} id="up">
+            UP
+          </button>
+          <button onClick={this.handleChangeRight} id="right">
+            RIGHT
+          </button>
+          <button onClick={this.handleChangeDown} id="down">
+            DOWN
+          </button>
+          <button onClick={this.resetState} id="reset">
+            reset
+          </button>
         </div>
-        <form>
-          <input value={this.state.email} 
-                  onChange={this.onChange}
-                  id="email" type="email" 
-                  placeholder="type email">
-          </input>
-
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.email}
+            onChange={this.handleEmail}
+            id="email"
+            type="email"
+            placeholder="type email"
+          ></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
-    )
+    );
   }
 }
+
+// // Write your tests here
+// import React from "react"
+// import { render, screen } from "@testing-library/react";
+// import AppClass from './AppClass';
+
+// test('sanity', () => {
+//   render(<AppClass/>)
+//   expect(true).not.toBe(false)
+// })
